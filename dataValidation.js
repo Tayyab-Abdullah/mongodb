a891601c1ac933e6f41448a373b6aafd7141fe01
Tayyab Abdullah <tabdullah0801@gmail.com>
2024-07-30 19:25:15 +0500

    Exception handling for data entry to database.

---
+++ dataValidation.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/dataValid')
+ .then(() => console.log('Connected to Database....'))
+ .catch((err) => console.log('Connection Failed to MongoDB: ', err.message));
+
+
+
+const courseSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    author: String,
+    tags: [ String ],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean,
+    price: Number
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+async function createCourse(){
+    const course = new Course({
+        // name: 'NodeJs Course',
+        author: 'Tayyab',
+        tags: ['Backend', 'JavaScript'],
+        isPublished: true,
+        price: 2000
+    });
+
+    try{
+        const result = await course.save();
+        console.log(result);
+    }
+    catch (ex){
+        console.log(ex.message);
+    }
+}
+
+createCourse();
