c4a3969a89daae054fbd1fcec3542a303be3bd16
Tayyab Abdullah <tabdullah0801@gmail.com>
2024-07-30 20:21:35 +0500

    Fixed an issue on mongoose.model

--- genres.js
+++ genres.js
@@ -4,7 +4,7 @@ const app = express();
 const Joi = require('joi');
 app.use(express.json());
 
-const Genre = new mongoose.model('Genre', new mongoose.Schema({
+const Genre = mongoose.model('Genre', new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -60,7 +60,7 @@ const Genre = new mongoose.model('Genre', new mongoose.Schema({
   // Handling delete request
   
   app.delete('/api/genres/:id', async (req, res) => {
-    
+
     const genre = await Genre.findByIdAndRemove(req.params.id);
     if (!genre) return res.status(404).send('The genre with the given ID was not found.');
 
